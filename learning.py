0a511307778386858d110734e5a81987ac0586cb
thasleema <thasleema.begum520@gmail.com>
2026-06-24 12:25:50 +0530

    remove non-python files2

--- learning.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver=webdriver.Firefox()
-driver.get("https:\\facebook.com")
-driver.find_element(By.NAME,'email').send_keys("thasleema.shaik520@gmail.com")
-driver.find_element(By.ID,'_R_1hmkqsqppb6amH1_').send_keys("Thass.@123")
-driver.find_element(By.CSS_SELECTOR,'[class="x1i10hfl xjbqb8w x1ejq31n x18oe1m7 x1sy0etr xstzfhl x972fbf x10w94by x1qhh985 x14e42zd x1ypdohk x3ct3a4 xdj266r x14z9mp xat24cr x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x16tdsg8 x1hl2dhg xggy1nq x1fmog5m xu25z0z x140muxe xo1y3bh x87ps6o x1lku1pv x1a2a7pz x9f619 x3nfvp2 xdt5ytf xl56j7k x1n2onr6 xh8yej3"]').submit()
-
