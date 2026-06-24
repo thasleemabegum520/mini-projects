9ef0c333281660fe96bb4376513a79a61079831e
thasleema <thasleema.begum520@gmail.com>
2026-06-24 19:20:06 +0530

    str changed to sentence

--- count_char_occrnc.py
+++ count_char_occrnc.py
@@ -1,13 +1,13 @@
-str=input("enter characters of your choice:")
+sentence=input("enter characters of your choice:")
 new_str=""
 
-for x in range(len(str)):
-    if str[x] in new_str:
+for x in range(len(sentence)):
+    if sentence[x] in new_str:
         continue
     else:
         num=0
-        for i in range(x,len(str)):
-            if str[x] in str[i]:
+        for i in range(x,len(sentence)):
+            if sentence[x] in sentence[i]:
                 num+=1
-        new_str=new_str+str[x]
-    print(str[x],":",num,end=';')
+        new_str=new_str+sentence[x]
+    print(sentence[x],":",num,end=';')
